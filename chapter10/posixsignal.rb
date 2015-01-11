ea1424036627470bd8c12656d7322c6574dd0bce
kubo39 <kubo39@gmail.com>
2015-01-12 07:46:51 +0900

    <unistd.h>と<signal.h>の一部の関数をFFIを用いて呼び出せるようにした

---
+++ chapter10/posixsignal.rb
@@ -0,0 +1,71 @@
+require 'ffi'
+
+
+SIG_BLOCK = 0
+SIG_UNBLOCK = 1
+SIG_SETMASK = 2
+
+module Unistd
+  extend FFI::Library
+  ffi_lib 'libc.so.6'
+
+  # unsigned int alarm(unsigned int);
+  attach_function :alarm, [:uint], :uint
+
+  # int pause(void);
+  attach_function :pause, [:void], :int
+end
+
+
+module SignalExt
+  extend FFI::Library
+  ffi_lib 'libc.so.6'
+
+  class Sigset < FFI::Struct
+    layout :__val, :ulong
+  end
+
+  # int sigprocmask(int, const sigset_t*, const sigset_t*);
+  attach_function :sigprocmask, [:int, :pointer, :pointer], :int
+
+  # int sigismember(const sigset_t*, int);
+  attach_function :sigismember, [:pointer, :int], :int
+
+  # int sigemptyset(sigset_t*);
+  attach_function :sigemptyset, [:pointer], :int
+
+  # int sigaddset(sigset_t*, int);
+  attach_function :sigaddset, [:pointer, :int], :int
+
+  # int sigpending(sigset_t*);
+  attach_function :sigpending, [:pointer], :int
+end
+
+
+module Signal
+  class SigprocmaskError < Exception; end
+
+  Sigset = SignalExt::Sigset
+
+  def self.sigprocmask(how, set, oldset)
+    if SignalExt.sigprocmask(how, set, oldset) < 0
+      raise SigprocmaskError
+    end
+  end
+
+  def self.sigismember(set, signo)
+    SignalExt.sigismember(set, signo)
+  end
+
+  def self.sigemptyset(set)
+    SignalExt.sigemptyset(set)
+  end
+
+  def self.sigaddset(set, signo)
+    SignalExt.sigaddset(set, signo)
+  end
+
+  def self.sigpending(set)
+    SignalExt.sigpending(set)
+  end
+end
